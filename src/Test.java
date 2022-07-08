6e62aaa3973232e19824dfb9d0975a3833dc22fd
GonulG <allygulsen@gmail.com>
2022-07-08 21:26:36 +0300

    This code is from Ugur

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+
+
+        int [] array ={1,2,3,4,5,6};
+        for(int i=0; i< array.length-1; i++){
+            System.out.println(array[i] + " ");
+        }
+    }
+}
